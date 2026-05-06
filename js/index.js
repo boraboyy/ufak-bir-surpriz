6dcccd536363a74072153db619a268cd34e30ce4
boraboyy <bora.yalcinkaya@hotmail.com>
2026-05-06 20:05:33 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Adın Burcu’ysa tıkla yoksa çık!'.split('')
+const text = 'Adın Sıla’ysa tıkla yoksa çık!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
